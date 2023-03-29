3edd518cec7a0f4d236380aa5123fbe6e41bf7e6
Replit user <>
2023-03-29 22:44:44 +0000

    new try

--- arraylist.c
+++ arraylist.c
@@ -12,38 +12,67 @@ typedef struct ArrayList {
 
 ArrayList *createList(void) 
 {
-  ArrayList * arreglo = (ArrayList *) malloc(sizeof(ArrayList));
-  arreglo->data = (void **) malloc(2 * sizeof(void*));
-  arreglo->capacity = 2;
-  arreglo->size = 5;
-
-  if(arreglo == NULL) exit(1);
-  
-  return arreglo;
+    ArrayList *list = (ArrayList *) malloc(sizeof(ArrayList));
+    assert(list != NULL);
+    list->data = (void **) malloc(sizeof(void *) * 10); // Inicialmente se reserva espacio para 10 elementos
+    assert(list->data != NULL);
+    list->capacity = 10;
+    list->size = 0;
+    return list;
 }
 
-void append(ArrayList * l, void * data)
+void append(ArrayList *l, void *data)
 {
-  
+    // Si no hay suficiente espacio en el arreglo, se duplica la capacidad
+    if (l->size == l->capacity) {
+        l->capacity *= 2;
+        l->data = (void **) realloc(l->data, sizeof(void *) * l->capacity);
+        assert(l->data != NULL);
+    }
+    l->data[l->size] = data;
+    l->size++;
 }
 
-void push(ArrayList * l, void * data, int i){
-
+void push(ArrayList *l, void *data, int i)
+{
+    // Si no hay suficiente espacio en el arreglo, se duplica la capacidad
+    if (l->size == l->capacity) {
+        l->capacity *= 2;
+        l->data = (void **) realloc(l->data, sizeof(void *) * l->capacity);
+        assert(l->data != NULL);
+    }
+    // Se desplazan los elementos a la derecha del índice i para hacer espacio para el nuevo elemento
+    for (int j = l->size; j > i; j--) {
+        l->data[j] = l->data[j-1];
+    }
+    l->data[i] = data;
+    l->size++;
 }
 
-void* pop(ArrayList * l, int i){
-    return NULL;
+void *pop(ArrayList *l, int i)
+{
+    void *data = l->data[i];
+    // Se desplazan los elementos a la izquierda del índice i para eliminar el elemento
+    for (int j = i; j < l->size-1; j++) {
+        l->data[j] = l->data[j+1];
+    }
+    l->size--;
+    return data;
 }
 
-void* get(ArrayList * l, int i){
-    return NULL;
+void *get(ArrayList *l, int i)
+{
+    assert(i >= 0 && i < l->size);
+    return l->data[i];
 }
 
-int get_size(ArrayList * l){
+int get_size(ArrayList *l)
+{
     return l->size;
 }
 
-//remove elements
-void clean(ArrayList * l){
-    
+void clean(ArrayList *l)
+{
+    free(l->data);
+    free(l);
 }
