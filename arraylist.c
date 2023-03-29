de8f81251b54909a4c7eda576bca562664d093ef
Replit user <>
2023-03-29 22:39:04 +0000

    new try

--- arraylist.c
+++ arraylist.c
@@ -16,6 +16,9 @@ ArrayList *createList(void)
   arreglo->data = (void **) malloc(2 * sizeof(void*));
   arreglo->capacity = 2;
   arreglo->size = 2;
+
+  if(arreglo == NULL) exit(1);
+  
   return arreglo;
 }
 
