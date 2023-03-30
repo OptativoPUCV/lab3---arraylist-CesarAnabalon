00b14b04737c005a4f1442a538d4bef6fb0655ec
Replit user <>
2023-03-30 20:13:54 +0000

    new try

--- arraylist.c
+++ arraylist.c
@@ -42,8 +42,14 @@ void append(ArrayList * l, void * data)
 void push(ArrayList * l, void * data, int i)
 {
   if(i > l->size) EXIT_FAILURE;
+  if(l->capacity == l->size)
+  {
+    l->capacity = 2 * l->capacity;
+    l->data = (void**) realloc(l->data,sizeof(void)*l->capacity);
+    l->data[l->size] = data;
+  }
   
-  
+  l->size++;
 }
 
 void* pop(ArrayList * l, int i){
