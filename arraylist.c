4b85bb1d2ebc4ae3a1065242c4dc0e4c91f9476e
Replit user <>
2023-03-30 20:24:02 +0000

    new try

--- arraylist.c
+++ arraylist.c
@@ -43,6 +43,11 @@ void push(ArrayList * l, void * data, int i)
 {
   if(i > l->size) EXIT_FAILURE;
   
+  if(l->capacity == l->size)
+  {
+    l->capacity = 2 * l->capacity;
+    l->data = (void**) realloc(l->data,sizeof(void)*l->capacity);
+  }
   
   
 }
